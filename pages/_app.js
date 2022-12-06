a94824256e012dd6efd6879c3457133e121f4d6a
Sahil pillania <sahilpillania88@gmail.com>
2022-12-06 11:09:50 +0530

    bootstrap added

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
+import Head from "next/head";
+import "bootstrap/dist/css/bootstrap.css";
 import Navbar from "../components/navbar";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
